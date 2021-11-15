dd1191c3c8f830f9d5061d8d270634a53584cb70
aodong.qin <ioperations.c@gmail.com>
2021-11-15 12:48:16 +0800

    add ranger pligins and arch install scripts

--- Install.sh
+++ Install.sh
@@ -1,22 +1,27 @@
 #!/bin/sh
 
 case `uname -s` in
-	Linux)
+    Linux)
 
-		if cat /etc/os-release | grep debian ;then
-			sudo apt install -y atool bsdtar unrar-free p7zip \
-				poppler-utils \
-				mupdf-tools exiftool transmission-gtk \
-				odt2txt w3m w3m-img ffmpegthumbnailer \
-				graphicsmagick-imagemagick-compat \
-				unzip highlight exiftool mediainfo \
-				libxext-dev
-			sudo pip3 install ueberzug
-		else
-			echo "not debian"
-		fi
-		break
-		;;
-	*)
-		echo "not a linux distribution"
-esac
+        if cat /etc/os-release | grep debian ;then
+            sudo apt install -y atool bsdtar unrar-free p7zip \
+                poppler-utils \
+                mupdf-tools exiftool transmission-gtk \
+                odt2txt w3m w3m-img ffmpegthumbnailer \
+                graphicsmagick-imagemagick-compat \
+                unzip highlight exiftool mediainfo \
+                libxext-dev
+            sudo pip3 install ueberzug
+        else if cat /etc/os-release | grep -i arch ; then
+            sudo pacman -S atool p7zip \
+                w3m mediainfo \
+                unzip highlight 
+            sudo pip3 install ueberzug
+        else
+                echo "not debian or arch"
+        fi
+            break
+            ;;
+    *)
+        echo "not a linux distribution"
+        esac
